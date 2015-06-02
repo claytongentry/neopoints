98e9637e8bc1a549c66567bd08d60def1d12cf43
cg3ntry <cg3ntry@gmail.com>
2015-06-02 16:12:44 -0500

    Primitive voting structure working

--- index.php
+++ index.php
@@ -5,73 +5,70 @@
 
 <div id = "voting_box"></div>
 
-<form id = "points">
-  <input type = "checkbox" name = "Greg" value = "Greg">Greg
-  <br/>
-  <input type = "checkbox" name = "Kendall" value = "Kendall">Kendall
-  <br/>
-  <input type = "checkbox" name = "Kurt" value = "Kurt">Kurt
-  <br/>
-  <input type = "checkbox" name = "Bilal" value = "Bilal">Bilal
-  <br/>
-  <input type = "checkbox" name = "Taylor B." value = "Taylor B.">Taylor B.
-  <br/>
-  <input type = "checkbox" name = "Victoria" value = "Victoria">Victoria
-  <br/>
-  <input type = "checkbox" name = "Tif" value = "Tif">Tif
-  <br/>
-  <input type = "checkbox" name = "Jacob" value = "Jacob">Jacob
-  <br/>
-  <input type = "checkbox" name = "Taylor F." value = "Taylor F.">Taylor F.
-  <br/>
-  <input type = "checkbox" name = "Kate" value = "Kate">Kate
-  <br/>
-  <input type = "checkbox" name = "Rachel" value = "Rachel">Rachel
-  <br/>
-  <input type = "checkbox" name = "Emma" value = "Emma">Emma
-  <br/>
-  <input type = "checkbox" name = "Hannah" value = "Hannah">Hannah
-  <br/>
-  <input type = "checkbox" name = "Candice" value = "Candice">Candice
-  <br/>
-  <input type = "checkbox" name = "Brenda" value = "Brenda">Brenda
-  <br/>
-  <input type = "checkbox" name = "Ida" value = "Ida">Ida
-  <br/>
-  <input type = "checkbox" name = "Trevor" value = "Trevor">Trevor
-  <br/>
-  <input type = "checkbox" name = "Nick" value = "Nick">Nick
-  <br/>
-  <input type = "checkbox" name = "Dan" value = "Dan">Dan
-  <br/>
-  <input type = "checkbox" name = "Lily" value = "Lily">Lily
-  <br/>
-  <input type = "checkbox" name = "Kirsten" value = "Kirsten">Kirsten
-  <br/>
-  <input type = "checkbox" name = "Leah" value = "Leah">Leah
-  <br/>
-  <input type = "checkbox" name = "Malic" value = "Malic">Malic
-  <br/>
-</form>
+<div id = "neos">
+    <form id = "points">
+      <input type = "checkbox" name = "Greg" value = "Greg">Greg
+      <input type = "checkbox" name = "Kendall" value = "Kendall">Kendall
+      <input type = "checkbox" name = "Kurt" value = "Kurt">Kurt
+      <input type = "checkbox" name = "Bilal" value = "Bilal">Bilal
+      <input type = "checkbox" name = "Taylor B." value = "Taylor B.">Taylor B.
+      <input type = "checkbox" name = "Victoria" value = "Victoria">Victoria
+      <input type = "checkbox" name = "Tif" value = "Tif">Tif
+      <input type = "checkbox" name = "Jacob" value = "Jacob">Jacob
+      <input type = "checkbox" name = "Taylor F." value = "Taylor F.">Taylor F.
+      <input type = "checkbox" name = "Kate" value = "Kate">Kate
+      <input type = "checkbox" name = "Rachel" value = "Rachel">Rachel
+      <input type = "checkbox" name = "Emma" value = "Emma">Emma
+      <input type = "checkbox" name = "Hannah" value = "Hannah">Hannah
+      <input type = "checkbox" name = "Candice" value = "Candice">Candice
+      <input type = "checkbox" name = "Brenda" value = "Brenda">Brenda
+      <input type = "checkbox" name = "Ida" value = "Ida">Ida
+      <input type = "checkbox" name = "Trevor" value = "Trevor">Trevor
+      <input type = "checkbox" name = "Nick" value = "Nick">Nick
+      <input type = "checkbox" name = "Dan" value = "Dan">Dan
+      <input type = "checkbox" name = "Lily" value = "Lily">Lily
+      <input type = "checkbox" name = "Kirsten" value = "Kirsten">Kirsten
+      <input type = "checkbox" name = "Leah" value = "Leah">Leah
+      <input type = "checkbox" name = "Malic" value = "Malic">Malic
+      <input type = "submit" value = "Vote" onclick = "squarify()"
+    </form>
+</div>
 
 <script src = "progressbar.min.js"></script>
 <script>
-window.onload = function onLoad() {
+
+function squarify() {
+  console.log("Squarify called.");
+  var form = document.getElementById("points");
+  console.log("Form: " + form);
+  var vote_count = loop_form(form);
+  console.log("Vote Count: " + vote_count);
+  show_square(vote_count);
+}
+
+function loop_form(form) {
+  var count = 0;
+  for (var i = 0, len = form.elements.length; i < len; i++) {
+    if (form.elements[i].checked == true)
+      count += 0.1;
+      console.log("Count: " + count);
+  }
+  return count;
+}
+
+function show_square(height) {
+
     var square = new ProgressBar.Square('#voting_box', {
         width: "150px",
         color: 'red',
         strokeWidth: 5,
-        duration: 3000,
-        easing: 'easeInOut',
-        fill: function(height) {
-          if (height > 0.3)
-            return 'black';
-          else
-            return 'red';
-        }
+        duration: 2000,
+        easing: 'easeInOut'
     });
 
-    square.animate(0.2);
+    square.animate(height);
+
 };
+
 </script>
 <?php require('templates/footer.php'); ?>
