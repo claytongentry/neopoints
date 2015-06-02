3e79771d40b32dd168c38b0fbb8f5be01c0dee71
cg3ntry <cg3ntry@gmail.com>
2015-06-02 17:01:36 -0500

    Chart sizes with votes

--- index.php
+++ index.php
@@ -3,36 +3,36 @@
 
 <h1>NeoPoints</h1>
 
-<div id = "voting_box"></div>
-
 <div id = "neos">
-    <form id = "points">
-      <input type = "checkbox" name = "Greg" value = "Greg">Greg
-      <input type = "checkbox" name = "Kendall" value = "Kendall">Kendall
-      <input type = "checkbox" name = "Kurt" value = "Kurt">Kurt
-      <input type = "checkbox" name = "Bilal" value = "Bilal">Bilal
-      <input type = "checkbox" name = "Taylor B." value = "Taylor B.">Taylor B.
-      <input type = "checkbox" name = "Victoria" value = "Victoria">Victoria
-      <input type = "checkbox" name = "Tif" value = "Tif">Tif
-      <input type = "checkbox" name = "Jacob" value = "Jacob">Jacob
-      <input type = "checkbox" name = "Taylor F." value = "Taylor F.">Taylor F.
-      <input type = "checkbox" name = "Kate" value = "Kate">Kate
-      <input type = "checkbox" name = "Rachel" value = "Rachel">Rachel
-      <input type = "checkbox" name = "Emma" value = "Emma">Emma
-      <input type = "checkbox" name = "Hannah" value = "Hannah">Hannah
-      <input type = "checkbox" name = "Candice" value = "Candice">Candice
-      <input type = "checkbox" name = "Brenda" value = "Brenda">Brenda
-      <input type = "checkbox" name = "Ida" value = "Ida">Ida
-      <input type = "checkbox" name = "Trevor" value = "Trevor">Trevor
-      <input type = "checkbox" name = "Nick" value = "Nick">Nick
-      <input type = "checkbox" name = "Dan" value = "Dan">Dan
-      <input type = "checkbox" name = "Lily" value = "Lily">Lily
-      <input type = "checkbox" name = "Kirsten" value = "Kirsten">Kirsten
-      <input type = "checkbox" name = "Leah" value = "Leah">Leah
-      <input type = "checkbox" name = "Malic" value = "Malic">Malic
+    <form role = "form" id = "points">
+      <label><input type = "checkbox" name = "Greg" value = "Greg">Greg</label>
+      <label><input type = "checkbox" name = "Kendall" value = "Kendall">Kendall</label>
+      <label><input type = "checkbox" name = "Kurt" value = "Kurt">Kurt</label>
+      <label><input type = "checkbox" name = "Bilal" value = "Bilal">Bilal</label>
+      <label><input type = "checkbox" name = "Taylor B." value = "Taylor B.">Taylor B.</label>
+      <label><input type = "checkbox" name = "Victoria" value = "Victoria">Victoria</label>
+      <label><input type = "checkbox" name = "Tif" value = "Tif">Tif</label>
+      <label><input type = "checkbox" name = "Jacob" value = "Jacob">Jacob</label>
+      <label><input type = "checkbox" name = "Taylor F." value = "Taylor F.">Taylor F.</label>
+      <label><input type = "checkbox" name = "Kate" value = "Kate">Kate</label>
+      <label><input type = "checkbox" name = "Rachel" value = "Rachel">Rachel</label>
+      <label><input type = "checkbox" name = "Emma" value = "Emma">Emma</label>
+      <label><input type = "checkbox" name = "Hannah" value = "Hannah">Hannah</label>
+      <label><input type = "checkbox" name = "Candice" value = "Candice">Candice</label>
+      <label><input type = "checkbox" name = "Brenda" value = "Brenda">Brenda</label>
+      <label><input type = "checkbox" name = "Ida" value = "Ida">Ida</label>
+      <label><input type = "checkbox" name = "Trevor" value = "Trevor">Trevor</label>
+      <label><input type = "checkbox" name = "Nick" value = "Nick">Nick</label>
+      <label><input type = "checkbox" name = "Dan" value = "Dan">Dan</label>
+      <label><input type = "checkbox" name = "Lily" value = "Lily">Lily</label>
+      <label><input type = "checkbox" name = "Kirsten" value = "Kirsten">Kirsten</label>
+      <label><input type = "checkbox" name = "Leah" value = "Leah">Leah</label>
+      <label><input type = "checkbox" name = "Malic" value = "Malic">Malic</label>
       <input type = "submit" value = "Vote" onclick = "squarify()"
     </form>
 </div>
+<div id = "voting_box"></div>
+
 
 <script src = "progressbar.min.js"></script>
 <script>
@@ -49,22 +49,25 @@ function squarify() {
 function loop_form(form) {
   var count = 0;
   for (var i = 0, len = form.elements.length; i < len; i++) {
-    if (form.elements[i].checked == true)
+    if (form.elements[i].checked == true) {
       count += 0.1;
       console.log("Count: " + count);
+    }
   }
   return count;
 }
 
+var square = new ProgressBar.Square('#voting_box', {
+    width: "150px",
+    color: 'red',
+    strokeWidth: 5,
+    duration: 2000,
+    easing: 'easeInOut'
+});
+
 function show_square(height) {
 
-    var square = new ProgressBar.Square('#voting_box', {
-        width: "150px",
-        color: 'red',
-        strokeWidth: 5,
-        duration: 2000,
-        easing: 'easeInOut'
-    });
+
 
     square.animate(height);
 
--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <link type = "text/css" rel = "stylesheet" href = "awesome-bootstrap-checkbox.css">
   <link type = "text/css" rel = "stylesheet" href = "main.css"/>
   <title>Neofuturists Points Calculator</title>
 </head>
